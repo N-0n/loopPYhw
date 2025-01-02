bc72c1e3ffb52cd8f4d3ff4930ee25b656bdb7c1
N-0n <noureenfatima.nen@gmail.com>
2025-01-03 00:11:18 +0500

    aaa

--- 1.py
+++ 1.py
@@ -1,18 +1,15 @@
-
-num=input("Enter a positive integer : ")
+num=int(input("Enter a positive integer : "))
 tot=0 
-while num>0:
-    x=len(num)
-    z=0
-    for count in range(1,x):
-        substr=0
-        substr=num(z)
-        tot=tot+int(substr)**int(substr)
-        count=count+1
-        num(z+1)
-    if tot==int(num):
-        print("The number is an Armstrong number")
-    else:
-        print("The number isn't an Armstrong number")
+
+x=len(str(num))
+z=0
+for count in range(0,x):
+     substr=0
+     substr=num[z]
+     tot=tot+(substr**substr)
+     num[z+1]
+
+if tot==num:
+    print("The number is an Armstrong number")
 else:
-    print("The integer should be positive.")
+    print("The number isn't an Armstrong number")
